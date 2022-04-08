b5cca224da10e7aa715445d76460e3f5d34cd6a9
Clepter <84567842+ClepterLT@users.noreply.github.com>
2022-04-08 17:46:42 +0300

    Initial commit

---
+++ framework/functions/framework-queries.php
@@ -0,0 +1,15 @@
+<?php
+
+// function macu4GetPostsByUserId($userId) {
+//     $queryArgs = array(
+//       'posts_per_page' => -1,
+//       'post_type' => 'application',
+//       'meta_key' => 'application_user_id',
+//       'meta_value' => $userId,
+//     );
+//     $query = new WP_Query( $queryArgs );
+  
+//     return $query;
+//   }
+  
+?>
---
+++ templates/contact-form.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Contact Form Page
+*/
+
+get_header(); ?>
+
+<main>
+
+<section class="contact-form">
+    Contact form
+</section>
+
+</main>
+
+<?php get_footer(); ?>
